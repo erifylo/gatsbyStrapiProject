b191c5b2febd206a8121ca6168219f0e302a491e
erifylo <erif.evangelou@gmail.com>
2020-12-30 11:01:14 +0100

    First commit

---
+++ src/components/Jobs.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Title from "./Title"
+import { FaAngleDoubleRight } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
+import { Link } from "gatsby"
+
+const query = graphql`
+  {
+    __typename
+    allStrapiJobs(sort: { fields: strapiId }) {
+      nodes {
+        company
+        date
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+const Jobs = () => {
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  console.log(jobs);
+  return <h2>jobs component</h2>
+}
+
+export default Jobs
